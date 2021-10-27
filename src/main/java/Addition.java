2ef286cd8ca6cdcaa1509a7fdca90ac429a17ab3
Ajin Raju <ajinrajtk04ann@gmail.com>
2021-10-27 18:41:59 +0530

    Add files

---
+++ src/main/java/Addition.java
@@ -0,0 +1,6 @@
+package com.tkh;
+public class Addition{
+    public int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/main/java/com/tkh/App.java
@@ -0,0 +1,16 @@
+package com.tkh;
+import org.apache.commons.lang3.math.NumberUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Addition addition= new Addition();
+        System.out.println( "1+1:"+addition.add(1,1) );
+        System.out.println(NumberUtils.toDouble("23"));
+    }
+}
